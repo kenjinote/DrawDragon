007a0329e76a02f9b16983b2b86aeb2f83e5d59b
kenjinote <kenjinote@DESKTOP-MUMGVTF>
2016-10-24 09:52:34 +0900

    add files.

---
+++ Source.cpp
@@ -0,0 +1,90 @@
+﻿#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#include <windows.h>
+
+TCHAR szClassName[] = TEXT("Window");
+
+void DrawDragon(HDC hdc, const LPPOINT lp, const LPPOINT b, int n)
+{
+	const int x = b->x - lp->x;
+	const int y = -(b->y - lp->y);
+	POINT p = { lp->x + (x + y) / 2,b->y + (x + y) / 2 };
+	if (n <= 0)
+	{
+		MoveToEx(hdc, lp->x, lp->y, 0);
+		LineTo(hdc, p.x, p.y);
+		MoveToEx(hdc, b->x, b->y, 0);
+		LineTo(hdc, p.x, p.y);
+	}
+	else
+	{
+		DrawDragon(hdc, lp, &p, n - 1);
+		DrawDragon(hdc, b, &p, n - 1);
+	}
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CREATE:
+		break;
+	case WM_SIZE:
+		break;
+	case WM_PAINT:
+		{
+			PAINTSTRUCT ps;
+			HDC hdc = BeginPaint(hWnd, &ps);
+			POINT P = { 400, 200 };
+			POINT Q = { 600, 600 };
+			DrawDragon(hdc, &P, &Q, 12);
+			EndPaint(hWnd, &ps);
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int nCmdShow)
+{
+	MSG msg;
+	WNDCLASS wndclass = {
+		CS_HREDRAW | CS_VREDRAW,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		0,
+		LoadCursor(0,IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW + 1),
+		0,
+		szClassName
+	};
+	RegisterClass(&wndclass);
+	HWND hWnd = CreateWindow(
+		szClassName,
+		TEXT("Dragon"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		0,
+		CW_USEDEFAULT,
+		0,
+		0,
+		0,
+		hInstance,
+		0
+	);
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
